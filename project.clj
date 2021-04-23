48e768659f700cfc63be7d7945629035cf835a5b
Byron Botha <byron@byrongibby.online>
2021-04-23 23:48:08 +0200

    Upgraded minor version of ojAlgo 48.4.2

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject ojalgo-clj "0.1.0-SNAPSHOT"
+(defproject ojalgo-clj "0.1.0"
   :description "A core.matrix implementation of oj! Algorithms"
   :url "https://github.com/byrongibby/ojalgo-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[net.mikera/core.matrix "0.62.0"]
                  [org.clojure/clojure "1.10.1"]
-                 [org.ojalgo/ojalgo "48.4.2-SNAPSHOT"]]
+                 [org.ojalgo/ojalgo "48.4.2"]]
   :aot [ojalgo-clj.lambda]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
